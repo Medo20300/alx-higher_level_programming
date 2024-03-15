1d7f4e26287237db23442ebd249a1a46263517d5
Medo20300 <medhatconcrete@gmail.com>
2024-03-15 14:11:44 -0700

    Med

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class City"""
-
+"""Lists states"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from relationship_state import Base, State
-
+from relationship_state import Base
 
 class City(Base):
-        """Module of a city"""
-            __tablename__ = "cities"
-                id = Column(Integer, primary_key=True)
+        """Class representing the states table"""
+            __tablename__ = 'cities'
+
+                id = Column(Integer, nullable=False, primary_key=True,
+                                        autoincrement=True, unique=True)
                     name = Column(String(128), nullable=False)
-                        state_id = Column(Integer, ForeignKey('states.id'))
+                        state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Defines a class State"""
-
+"""Lists states"""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
 
 class State(Base):
-        """Module of a state"""
+        """Class representing the states table"""
             __tablename__ = 'states'
-                id = Column(Integer, primary_key=True)
+
+                id = Column(Integer, nullable=False, primary_key=True,
+                                        autoincrement=True, unique=True)
                     name = Column(String(128), nullable=False)
-                        cities = relationship("City", backref="state")
+
+                        cities = relationship(
+                                        "City",
+                                                cascade="all, delete-orphan",
+                                                        backref=backref("state", cascade="all"),
+                                                                single_parent=True)
